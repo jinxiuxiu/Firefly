b456dfbffad687d78147ac8fae0f5124737b8142
shanquan <323331434@qq.com>
2026-09-18 20:11:22 +0800

    名字

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "XiaYe",
+	name: "瑾锈锈",
 
 	// 个人签名
-	bio: "Hello, I'm XiaYe.",
+	bio: "一个无聊的人吧",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
